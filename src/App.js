41058e8e031f90ee708d491c7e9bf9bf178a1e3d
Saif <saif@Arifs-MBP-4.lan>
2023-10-12 14:15:07 -0400

    Deleting records

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import BookList from './components/BookList';
 function App() {
     const [books, setBooks] = useState([]);
 
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id;
+        });
+
+        setBooks(updatedBooks);
+    };
+
     const createBook = (title) => {
         // Array of updated books
         const updatedBooks = [...books,{id: Math.round(Math.random() * 999), title}];
@@ -13,7 +21,7 @@ function App() {
 
     return (
         <div className= "app">
-            <BookList books={books} />
+            <BookList books={books} onDelete={deleteBookById} />
             <BookCreate onCreate={createBook} />
         </div>
     ); 
