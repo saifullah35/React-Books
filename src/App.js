f5742b863621cccfcd8b17f2365785ca004b91d1
Saif <saif@Arifs-MBP-4.lan>
2023-10-12 14:04:05 -0400

    Generating random id's and displaying the list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App() {
     const [books, setBooks] = useState([]);
 
     const createBook = (title) => {
-        console.log('Need to add book with:',title);
+        // Array of updated books
+        const updatedBooks = [...books,{id: Math.round(Math.random() * 999), title}];
+        setBooks(updatedBooks);
     };
 
-
-
     return (
-        <div>
+        <div className= "app">
+            <BookList books={books} />
             <BookCreate onCreate={createBook} />
         </div>
     ); 
