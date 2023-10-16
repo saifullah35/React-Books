7809bf0b8c420937caeeb9e354a43ccde204d4a4
Saif <saif@Arifs-MBP-4.lan>
2023-10-16 17:32:04 -0400

    Resuable Hooks

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,10 @@
-import { useContext } from 'react';
-import BooksContext from '../context/books';
 import BookShow from './BookShow';
+import useBooksContext from '../hooks/use-books-context';
 
 function BookList() {
-    const { books } = useContext(BooksContext);
+    const { books } = useBooksContext();
 
-
-    const renderedBooks = book.map((book) => {
+    const renderedBooks = books.map((book) => {
         return<BookShow key={book.id} book={book} />;
     });
 
