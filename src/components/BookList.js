ddc051f31eb4863bf4ee71602cd4e3660198b894
Saif <saif@Arifs-MBP-4.lan>
2023-10-16 16:49:56 -0400

    Inroducing Context System and its values

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,18 @@
+import { useContext } from 'react';
+import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
 function BookList({ book, onDelete, onEdit }) {
+    const { count, incrementCount }= useContext(BooksContext);
+
     const renderedBooks = book.map((book) => {
         return<BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />;
     });
-    return <div className= "book-list">{renderedBooks}</div>;
+    return <div className= "book-list">
+        {count}
+        <button onClick= {incrementCount}>Click</button>
+        {renderedBooks}
+        </div>;
 }
 
 export default BookList;
